5603d4a0048acb395b034162531d5a0b764dd66b
tupsi07 <sanjida657@yahoo.com>
2025-01-21 11:20:03 +0600

    chatbox ui

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,17 @@ import logo from "./logo.png";
 import search from "./search.png";
 import profile_img from "./profile_img.png";
 import three_dot from "./three-dot.png";
+import img_attachment from "./img_attachment.png";
+import help from "./help.png";
+import arrow from "./arrow.png";
 
-export { logoBig, logo, search, profile_img, three_dot };
+export {
+  logoBig,
+  logo,
+  search,
+  profile_img,
+  three_dot,
+  img_attachment,
+  help,
+  arrow,
+};
--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./ChatBox.css";
+import { profile_img, img_attachment, arrow, help } from "../../assets";
 
 function ChatBox() {
-  return <div></div>;
+  return (
+    <div className="chat-box ">
+      <div className="chat-user">
+        <img src={profile_img} alt="" />
+        <p>GreatStack </p>
+        <img className="arrow" src="" alt="" />
+        <img className="help" src={help} alt="" />
+      </div>
+      <div className="chat-msg">
+        <div></div>
+      </div>
+      <div className="chat-input">
+        <input type="text" placeholder="Send a message" />
+        <label htmlFor="image">
+          <img src={img_attachment} alt="" />
+        </label>
+        <img src={arrow} alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default ChatBox;
