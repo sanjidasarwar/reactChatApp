bfeb943c0204b43395a03ebb62490a63364b1064
tupsi07 <sanjida657@yahoo.com>
2025-01-22 11:12:45 +0600

    image upload in updateProfile page functionality

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProfileUpdate.css";
 import { avator_icon, profile_icon } from "../../assets";
 
 function ProfileUpdate() {
+  const [image, setImage] = useState(null);
+
   return (
     <div className="profile">
       <div className="profile-container">
         <form>
           <h3>Profile details</h3>
           <label htmlFor="avatar">
-            <input id="avatar" type="file" accept=".png, .jpg, .jpeg" hidden />
-            <img src={avator_icon} alt="" />
+            <input
+              onChange={(e) => setImage(e.target.files[0])}
+              id="avatar"
+              type="file"
+              accept=".png, .jpg, .jpeg"
+              hidden
+            />
+            <img
+              src={image ? URL.createObjectURL(image) : avator_icon}
+              alt=""
+            />
             upload profile image
           </label>
           <input placeholder="Your name" type="text" required />
           <textarea placeholder="Write profile bio" required></textarea>
           <button type="submit">Save</button>
         </form>
-        <img className="profile-pic" src={profile_icon} alt="" />
+        <img
+          className="profile-pic"
+          src={image ? URL.createObjectURL(image) : profile_icon}
+          alt=""
+        />
       </div>
     </div>
   );
